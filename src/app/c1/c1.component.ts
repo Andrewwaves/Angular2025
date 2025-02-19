f4f687f332c7a85974124113770835b2a8b5c2df
Andrewwaves <andreacanalesmartin2000@gmail.com>
2025-02-19 09:05:34 +0100

    Add files via upload

---
+++ src/app/c1/c1.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-c1',
+  imports: [],
+  templateUrl: './c1.component.html',
+  styleUrl: './c1.component.css'
+})
+export class C1Component {
+
+  nombre:String="Andrea";
+  
+}
